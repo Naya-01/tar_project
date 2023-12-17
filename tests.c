520f457266996795349fd70b0da3e7a1baaa1065
cedricniyi <cedric.niyikiza@student.uclouvain.be>
2023-12-17 22:24:34 +0000

    add - testing for readfile

--- tests.c
+++ tests.c
@@ -39,7 +39,7 @@ int main(int argc, char **argv) {
     // int ret = check_archive(fd);
     // printf("check_archive returned %d\n", ret);
 
-    char *test = "copyfolder";
+    char *test = "dossier/symb.txt";
 
     int ret0 = exists(fd, test);
     printf("exists returned %d\n", ret0);
@@ -63,6 +63,14 @@ int main(int argc, char **argv) {
     int ret4 = list(fd, test, entries, &no_entries);
     printf("list returned %d\n", ret4);
 
+    printf("==========\n");
 
+    uint8_t buffer[256];
+    size_t len = sizeof(buffer);
+    int ret5 = read_file(fd, test,0, buffer, &len);
+    printf("readfile returned %d\n", ret5);
+    if(ret5>=0){
+        printf("buffer content : %s\n-----\n", buffer);
+    }
     return 0;
 }
